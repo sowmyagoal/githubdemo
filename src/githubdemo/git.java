c13799030ce07f3706c6fddd962770b4f25789f2
Sowmya <Sowmya@DESKTOP-U6M3JIM>
2021-12-12 21:22:21 +0530

    practice session

---
+++ src/githubdemo/git.java
@@ -0,0 +1,8 @@
+package githubdemo;
+
+public class git {
+	
+	public static void main(String[] args) {
+System.out.println("Gitdemo");
+	}
+}
---
+++ src/githubdemo/gitpractice.java
@@ -0,0 +1,10 @@
+package githubdemo;
+
+public class gitpractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Practice");
+		
+	}
+}
